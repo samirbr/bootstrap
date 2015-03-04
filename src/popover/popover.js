93c247b41b9f8c4457470ca8ea761cee4ef55ca1
Samir El Aouar <samirbr@gmail.com>
2015-03-04 14:56:43 -0800

    Update popover.js
    Changes to accept UnsafeHtml.

--- src/popover/popover.js
+++ src/popover/popover.js
@@ -16,4 +16,22 @@ angular.module( 'ui.bootstrap.popover', [ 'ui.bootstrap.tooltip' ] )
 
 .directive( 'popover', [ '$tooltip', function ( $tooltip ) {
   return $tooltip( 'popover', 'popover', 'click' );
+}])
+
+.directive("popoverHtmlUnsafe", ["$tooltip", function($tooltip) {
+    return $tooltip( 'popoverHtmlUnsafe', 'popover', 'click' );
 }]);
+
+angular.module("template/popover/popover-html-unsafe.html", [])
+    .run(["$templateCache", function($templateCache) {
+        templateCache.put(
+            "template/popover/popover-html-unsafe.html",
+            '<div class="popover {{placement}}" ng-class="{ in: isOpen(), fade: animation() }">\
+                <div class="arrow"></div>\
+                \
+                <div class="popover-inner">\
+                    <h3 class="popover-title" ng-bind="title" ng-show="title"></h3>\
+                    <div class="popover-content" bind-html-unsafe="content"></div>\
+                </div>\
+            </div>');
+    }]);
